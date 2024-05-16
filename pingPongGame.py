86fc89fc7517c923bb349b3e2ef4e574c8a9e203
SergeyBorozdin <94010348+SergeyBorozdin@users.noreply.github.com>
2024-05-16 21:55:20 +0300

    Update pingPongGame.py
    next commit

--- pingPongGame.py
+++ pingPongGame.py
@@ -1,11 +1,11 @@
 import turtle
+from random import choice, randint
 
 # размер и заголовок диалогового окна
 window = turtle.Screen()
 window.title("Ping-Pong")
 window.setup(width=1.0, height=1.0)
 window.bgcolor("black")
-
 
 # размеры  и отрисовка поля
 border = turtle.Turtle()
@@ -18,10 +18,10 @@ border.goto(-500,-300)
 border.goto(500,-300)
 border.goto(500,300)
 border.end_fill()
-
 border.goto(0,300)
 border.color('white')
 border.setheading(270)
+
 # рисуем пунктир
 for i in range(25):
     if i%2==0:
@@ -33,7 +33,6 @@ for i in range(25):
 border.hideturtle()
 
 # рисуем ракетки
-
 rocket_p1 =turtle.Turtle()
 rocket_p1.color("white")
 rocket_p1.shape("square")
@@ -73,12 +72,44 @@ def move_down_p2():
         y = -250
     rocket_p2.sety(y)
 
-# управление ракетками
+# рисуем мяч
+ball = turtle.Turtle()
+ball.shape("circle")
+ball.color("red")
+ball.speed(0)
+
+# задаем скорость движения
+ball.dx = 3
+ball.dy = -3
+ball.penup()
+
+# Назначение клавиш ракеток
 window.listen()
 window.onkeypress(move_up_p1, "w")
 window.onkeypress(move_down_p1, "s")
-window.onkeypress(move_up_p2, "o")
-window.onkeypress(move_down_p2, "l")
+window.onkeypress(move_up_p2, "Up")
+window.onkeypress(move_down_p2, "Down")
+
+# задаем параметра движения мяча
+while True:
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    if ball.ycor() >= 290:
+        ball.dy = -ball.dy
+
+    if ball.ycor() <= -290:
+        ball.dy = -ball.dy
+
+    if ball.xcor() >= 490:
+        ball.goto(0,randint(-150,150))
+        ball.dx = choice([-4,-3,-2,2,3,4])
+        ball.dy = choice([-4,-3,-2,2,3,4])
+
+    if ball.xcor() <= -490:
+        ball.goto(0,randint(-150,150))
+        ball.dx = choice([-4,-3,-2,2,3,4])
+        ball.dy = choice([-4,-3,-2,2,3,4])
 
 
 window.mainloop()
